391445715b4f645853313d9eedff56f4cbef09ab
Petya Djanovska <petya.djanovska@gmail.com>
2018-04-19 14:02:13 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,4 +3,8 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
   has_many :genres
 
+  def build_network(call_letters)
+    self.network = Network.new(call_letters)
+  end
+
 end
